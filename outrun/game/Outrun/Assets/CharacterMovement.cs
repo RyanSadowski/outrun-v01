8278c43f829510814f9c38b4dfc151b393c8079b
namo <nemosaurus@gmail.com>
2016-09-07 23:58:34 -0700

    added movement, dialog boxes

---
+++ outrun/game/Outrun/Assets/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	Rigidbody rigidbody3D;
+	public float speed = 4;
+	public float jump = 0f;
+	private float vertMove = 0f;
+
+	void Start () 
+	{
+		
+		rigidbody3D = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate () 
+	{
+		vertMove = 0;	
+
+		// Checks for user inputs.
+		float horizontal = Input.GetAxis("Horizontal");
+		if (Input.GetKeyDown ("space")) {
+			
+			vertMove = jump;
+
+		}
+
+
+
+		Vector3 movement = new Vector3(horizontal, vertMove, 0);
+
+		// Makes the player move.
+		rigidbody3D.AddForce(movement * speed / Time.deltaTime);
+
+
+		// Limits the speed of the rigidbody.
+		if (rigidbody3D.velocity.magnitude > speed)
+		{
+			rigidbody3D.velocity = rigidbody3D.velocity.normalized * speed;
+		}
+
+
+
+
+	}
+}
---
+++ outrun/game/Outrun/Assets/move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update()
+	{
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		rb.AddForce (movement * thrust);
+
+	}
+}
+
+
